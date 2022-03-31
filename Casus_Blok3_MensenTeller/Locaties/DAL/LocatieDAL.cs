393e60076e95e000155b133c5b181c58d995c48a
VyxMain <vyxisprime@gmail.com>
2022-03-31 12:06:32 +0200

    Created LocatieDal
    LocatieDal Eerste opzet vsan Functies

---
+++ Casus_Blok3_MensenTeller/Locaties/DAL/LocatieDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus_Blok3_MensenTeller.Locaties.DAL
+{
+    public class LocatieDAL
+    {
+        private string conString = "";
+
+        public void Create(Locatie l)
+        {
+
+        }
+        public Locatie Get(int id)
+        {
+            Locatie l = new Locatie();
+
+            return l;
+        }
+        public void Update(Locatie l)
+        {
+
+        }
+        public void Delete(Locatie l)
+        {
+
+        }
+    }
+}
