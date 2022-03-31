e0590b95082cc55cc716a146c7c87fab8484f428
VyxMain <vyxisprime@gmail.com>
2022-03-31 12:00:23 +0200

    Created Locatie Object
    Locatie klass gemaakt

---
+++ Casus_Blok3_MensenTeller/Locaties/Locatie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus_Blok3_MensenTeller.Locaties
+{
+    public class Locatie
+    {
+        public int ID { get; set; }
+        public string Adres { get; set; }
+        public int Nummer { get; set; }
+        public string Toevoeging { get; set; }
+        public string Plaats { get; set; }
+        public string PostCode { get; set; }
+
+    }
+}
