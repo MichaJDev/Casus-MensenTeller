ecff7c68da4e0d39b0406bfd5195009e1c1d6011
VyxMain <vyxisprime@gmail.com>
2022-04-01 08:27:29 +0200

    #Ramon #2

---
+++ Casus_Blok3_MensenTeller/Sensors/Sensor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus_Blok3_MensenTeller.Sensors
+{
+    public class Sensor
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Sensor()
+        {
+
+        }
+        public Sensor(int id)
+        {
+            ID = id;
+        }
+
+        public Sensor(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
---
+++ Casus_Blok3_MensenTeller/Toestellen/Toestel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Casus_Blok3_MensenTeller.Sensors;
+
+namespace Casus_Blok3_MensenTeller.Toestellen
+{
+    public class Toestel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Sensor Sensor { get; set; }
+
+        public Toestel()
+        {
+
+        }
+        public Toestel(int id)
+        {
+            ID = id;
+        }
+        public Toestel(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+        public Toestel(int id, string name, Sensor sensor)
+        {
+            ID = id;
+            Name =name; 
+            Sensor = sensor;
+        }
+    }
+}
