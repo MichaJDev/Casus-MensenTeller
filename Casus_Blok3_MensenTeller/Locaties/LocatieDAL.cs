bf5500d642f5ad364a7214684ffe8d7dadb98457
Rubberduckx <57198327+Rubberduckx@users.noreply.github.com>
2022-04-06 14:55:39 +0200

    DAL Locaties aangepast

--- Casus_Blok3_MensenTeller/Locaties/LocatieDAL.cs
+++ Casus_Blok3_MensenTeller/Locaties/LocatieDAL.cs
@@ -17,13 +17,12 @@ namespace Casus_Blok3_MensenTeller.Locaties
         {
             using (SqlConnection con = new SqlConnection(conString))
             {
-                string query = "INSERT INTO Locaties (Adres,Nummer,Toevoeging,Plaats,Postcode) VALUES (@adres, @nummer,@toevoeging,@plaats,@postcode)";
+                string query = "INSERT INTO Locaties (Name,Description,Xco,Yco) VALUES (@name, @description,@xco,@yco)";
                 SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@adres", l.Adres);
-                cmd.Parameters.AddWithValue("@nummer", l.Nummer);
-                cmd.Parameters.AddWithValue("@toevoeging", l.Toevoeging);
-                cmd.Parameters.AddWithValue("@plaats", l.Plaats);
-                cmd.Parameters.AddWithValue("@postcode", l.PostCode);
+                cmd.Parameters.AddWithValue("@adres", l.Name);
+                cmd.Parameters.AddWithValue("@nummer", l.Description);
+                cmd.Parameters.AddWithValue("@toevoeging", l.Xco);
+                cmd.Parameters.AddWithValue("@plaats", l.Yco);
                 con.Open();
                 try
                 {
@@ -41,14 +40,14 @@ namespace Casus_Blok3_MensenTeller.Locaties
             Locatie l = new Locatie();
             using (SqlConnection con = new SqlConnection(conString))
             {
-                string query = "SELECT * FROM Locaties WHERE vestigingId = @id";
+                string query = "SELECT * FROM Locaties WHERE sensorId = @id";
                 SqlCommand cmd = new SqlCommand(query, con);
                 cmd.Parameters.AddWithValue("@id", id);
                 con.Open();
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    return new Locatie(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetString(3), reader.GetString(4), reader.GetString(5));
+                    return new Locatie(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
                 }
             }
             return l;
@@ -57,13 +56,12 @@ namespace Casus_Blok3_MensenTeller.Locaties
         {
             using (SqlConnection con = new SqlConnection(conString))
             {
-                string query = "UPDATE Locaties SET Adres = @adres, Nummer = @nummer, Toevoeging = @toevoeging, Plaats = @plaats, Postcode = @postcode WHERE Id = @id";
+                string query = "UPDATE Locaties SET Name = @name, Description = @description, Xco = @xco, Yco = @yco WHERE Id = @id";
                 SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@adres", l.Adres);
-                cmd.Parameters.AddWithValue("@nummer", l.Nummer);
-                cmd.Parameters.AddWithValue("@toevoeging", l.Toevoeging);
-                cmd.Parameters.AddWithValue("@plaats", l.Plaats);
-                cmd.Parameters.AddWithValue("@postcode", l.PostCode);
+                cmd.Parameters.AddWithValue("@name", l.Name);
+                cmd.Parameters.AddWithValue("@description", l.Description);
+                cmd.Parameters.AddWithValue("@xco", l.Xco);
+                cmd.Parameters.AddWithValue("@yco", l.Yco);
                 cmd.Parameters.AddWithValue("@id", l.ID);
                 con.Open();
                 try
