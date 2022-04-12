44a0cc766f19c828139253cf6ff98dfcda37ca19
MichaJDev <michajanssen93@gmail.com>
2022-04-12 02:21:38 +0200

    Update DAL.cs in Simulation
    DAL.CS heeft nu een Lijst van beide data die gegenereerd kan worden door daal aan te maken in een andere class (DAL dal = new DAL();)  wordt data automatisch in de lists EntreeSensors en DrukSensors geladen om uit te kunnen lezen

--- Mensenteller_B3/Simulation_DAL/DAL.cs
+++ Mensenteller_B3/Simulation_DAL/DAL.cs
@@ -13,28 +13,57 @@ namespace MensenTeller_B3.Simualtion_DAL
 {
     class DAL
     {
-        public List<DrukSensor> DrukSensorsSim { get; set; }
-        public List<EntreeSensor> EntreeSensorsSim { get; set; }
+        public List<DrukSensor> DrukSensors { get; set; }
+        public List<EntreeSensor> EntreeSensors { get; set; }
         private string source = ".";
         private string catalog = "SensorData";
 
         const int MAXRANDOM = 8; // maximum number of people to add or delete per datageneration
         const int INTERVALMINUTES = 5; // interval in minutes to generate data for
 
+        public DAL()
+        {
+            ReadDrukSimulator();
+            ReadEntreeSensors();
+        }
         private string GetConnectionString()
         {
             return $"Data Source=" + source + ";Initial Catalog=" + catalog + ";Integrated Security=True";
         }
 
         //Haal op vanuit SensorData Db maken nog
-        public void ReadDrukSimulator()
+        private void ReadDrukSimulator()
         {
-            //TODO Table PressureSensors
+            using(SqlConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                string sql = "SELECT * FROM PressureSensors";
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        DrukSensors.Add(new DrukSensor(reader.GetInt32(0), reader.GetInt32(1), reader.GetBoolean(2),reader.GetString(3)));
+                    }
+                }
+            }
         }
 
-        public void ReadEntreeSensors()
+        private void ReadEntreeSensors()
         {
-            //TODO Table SensorReading
+            using (SqlConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                string sql = "SELECT * FROM SensorReading";
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        EntreeSensors.Add(new EntreeSensor(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3),reader.GetString(4)));
+                    }
+                }
+            }
         }
     }
 }
