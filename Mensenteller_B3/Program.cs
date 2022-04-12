040578fc08a75adb2f73ed20f6ffad06dc5969b0
Rubberduckx <57198327+Rubberduckx@users.noreply.github.com>
2022-04-12 12:10:34 +0200

    Simulatie
    Ophalen van sensor data, en verwerken.
    DrukSensorDAL aangepast

--- Mensenteller_B3/Program.cs
+++ Mensenteller_B3/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Mensenteller_B3.Sensors.DrukSensors;
+using MensenTeller_B3.Simualtion_DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MensenTeller_B3.Sensors;
+using Mensenteller_B3.Sensors.EntreeSensors;
 
 namespace Mensenteller_B3
 {
@@ -17,6 +21,20 @@ namespace Mensenteller_B3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+
+            DAL dal = new DAL();
+            DruksensorDAL dsDal = new DruksensorDAL();
+            foreach (DrukSensor ds in dal.DrukSensors)
+            {
+               dsDal.CreateDruksensor(ds);
+            }
+            //Dit is voor ALLE EntreeSensors van de gehele dag opteslaan
+            EntreeSensorDAL esDal = new EntreeSensorDAL();
+
+            foreach (EntreeSensor ds in dal.EntreeSensors)
+                esDal.Create(ds);
         }
     }
 }
+    
+
